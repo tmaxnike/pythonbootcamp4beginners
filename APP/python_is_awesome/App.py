e957906909838e54bfdc2720be5f78ac28f8fb2d
tmaxnike <tmaxnike@yahoo.com>
2023-05-12 19:50:37 -0500

    simple interest

---
+++ APP/python_is_awesome/App.py
@@ -0,0 +1,17 @@
+#Simple Interest= P X T X R
+#Where P is the principal amount, 
+#R is the rate of the interset,
+#and the T is the total time of interest on the amount
+
+
+# Simple interest in python
+# Enter the amount
+Principal = 1000
+# Enter the number of years
+Year = 12
+# Enter the rate of interest
+Rate = 1.2
+# Calculate the simple interest
+SI = Principal*Year*Rate
+print("The simple interest is:", SI)
+
